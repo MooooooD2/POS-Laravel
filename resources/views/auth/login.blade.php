e7d8e4d4a0fc73595c7f08b5b783967c72bf8908
mahmoud elshafie <mg01004428309@gmail.com>
2026-05-10 19:59:39 +0300

    `Added @nonce attribute to script tag`

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -118,7 +118,7 @@
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-    <script>window.LOGIN_URL = "{{ route('login.post') }}";</script>
+    <script @nonce>window.LOGIN_URL = "{{ route('login.post') }}";</script>
     <script src="{{ asset('js/login.js') }}"></script>
 </body>
 </html>
